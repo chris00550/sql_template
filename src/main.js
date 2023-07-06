37c0184622a5edf40a1b76e74416b5d23c1920f8
chris00550 <132188783+chris00550@users.noreply.github.com>
2023-07-06 15:38:32 +0800

    0706

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
+const { rename } = require("node:fs");
 const path = require("node:path");
 
 const db = new sqlite3.Database(path.join(__dirname, "sample.db"), sqlite3.OPEN_READWRITE, (error) => {
@@ -33,6 +34,43 @@ function CallbackFunc(Error, Results){
     Results.forEach(data => console.log(data));
 }
 
-let sql = ``
+let sql1 = `
 
-db.exec(sql, CallbackFunc);
+CREATE TABLE IF NOT EXISTS Users(
+    ID TEXT PRIMARY KEY,
+    Username TEXT,
+    Email TEXT,
+    DOB TEXT
+);
+`
+let sql2 = `SELECT name FROM pragma_table_info("Users")
+
+`
+let c=`ALTER TABLE  Users
+RENAME COLUMN email to Emali;`
+
+let d=`INSERT INTO Users(ID, Username, Emali, DOB)
+VALUES("13581","BOB","BOB@csis.com","2005-10-28");`
+
+let e = `SELECT * FROM Users
+WHERE Username = "BOBs";
+`
+let f = `UPDATE UserS 
+SET Username = "BOBs" 
+WHERE ID="13581";`
+
+let g = `DELETE FROM Users
+WHERE Username="BOBs";`
+//db.all(c, CallbackFunc);
+
+//db.all(sql2, CallbackFunc);
+
+//db.all(d, CallbackFunc);
+
+//db.all(sql2, CallbackFunc);
+
+//db.all(e, CallbackFunc);
+
+//db.exec(f, CallbackFunc);
+
+db.all(g, CallbackFunc);
